837fd4f91b4ce46eb465f05b5bfdf3d1d4271ed1
alextaikato <alextaikato@users.noreply.github.com>
2016-04-30 01:15:01 +1200

    Update Store.java

--- Store.java
+++ Store.java
@@ -15,16 +15,17 @@ public class Store{
     public boolean draw=false;
     /**      */
     public Store(){
-        // UI.initialise();
+        UI.initialise();
         //UI.addButton("Quit", UI::quit);
         
 
-        //drawStore();
+        drawStore();
 
     }
     //drawing the store
     public void drawStore(){
-    
+            int height=120;
+            int width=100;
             UI.setMouseListener(this::doMouse);
             UI.clearGraphics();
             UI.setColor(Color.black);
@@ -32,13 +33,13 @@ public class Store{
             UI.setColor(Color.blue);
             UI.setFontSize(50);
             UI.drawString("Heist",300, 20);
-            UI.drawRect(50,50,100,120);
-            UI.drawRect(50,200,100,120);
-            UI.drawRect(250,50,100,120);
-            UI.drawRect(250,200,100,120);
-            UI.drawRect(400,50,100,120);
-            UI.drawRect(400,200,100,120);
-            UI.drawRect(250,350,100,120);
+            UI.drawRect(50,50,width,120);
+            UI.drawRect(50,200,width,height);
+            UI.drawRect(250,50,width,height);
+            UI.drawRect(250,200,width,height);
+            UI.drawRect(400,50,width,height);
+            UI.drawRect(400,200,width,height);
+            UI.drawRect(250,350,width,height);
             UI.setFontSize(20);
             UI.drawString("Cash: $"+Cash, 600, 50);
             UI.drawString("stealth: "+numStealth,600,100);
@@ -55,23 +56,25 @@ public class Store{
         if(action.equals("clicked")){
             xClick= x;
             yClick=y;
-            draw=true;
+            
         }
-        if(xClick>50 && xClick<150 && yClick>50 && yClick>50+120 && Cash>49){
+        if(xClick>50 && xClick<150 && yClick>50 && yClick<50+120 && Cash>49){
             numStealth=numStealth+1;
             Cash=Cash-50;
 
-        }else if(xClick>50 && xClick<150 && yClick>200 && yClick>200+120 && Cash>49){
+        }else if(xClick>50 && xClick<150 && yClick>200 && yClick<200+120 && Cash>49){
+            
+        }else if(xClick>250 && xClick<350 && yClick>50 && yClick<50+120 && Cash>49){
             numStrong=numStrong+1;
             Cash=Cash-50;
-        }else if(xClick>250 && xClick<350 && yClick>50 && yClick>50+120 && Cash>49){
-            numRange=numRange+1;
-            Cash=Cash-50;
-        }else if(xClick>250 && xClick<350 && yClick>200 && yClick>200+120){
+           
+        }else if(xClick>250 && xClick<350 && yClick>200 && yClick<200+120){
 
-        }else if(xClick>400 && xClick<550 && yClick>50 && yClick>50+120){
-        }else if(xClick>400 && xClick<550 && yClick>200 && yClick>200+120){}
-        else if(xClick>250 && xClick<350 && yClick>350 && yClick>350+120){
+        }else if(xClick>400 && xClick<500 && yClick>50 && yClick<50+120){
+             numRange=numRange+1;
+            Cash=Cash-50;
+        }else if(xClick>400 && xClick<550 && yClick>200 && yClick<200+120){}
+        else if(xClick>250 && xClick<350 && yClick>350 && yClick<350+120){
             draw= true;
         }
         if(draw==false){
