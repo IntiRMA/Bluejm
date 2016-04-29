94ad09c1d84b87ebc2d9463f1ceb89aa2826521c
IntiMRA <inti.resende@gmail.com>
2016-04-29 20:07:48 +1200

    Added files via upload

---
+++ Characters.java
@@ -0,0 +1,29 @@
+/** <description of class Characters>
+  */
+ public class Characters{
+     private String type;
+    private double width;
+    private double height;
+    private int speed;
+    private double welth;
+    private String life;
+    
+    
+ 
+     /**      */
+    public Characters(){
+        UI.initialise();
+    UI.addButton("Quit", UI::quit);
+    public Characters(String typ,double x,double y){
+       
+    }
+    private void draw(){
+    }
+    
+     }
+ 
+ 
+ 
+
+ 
+ }
